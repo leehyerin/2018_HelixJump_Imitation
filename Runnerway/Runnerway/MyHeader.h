e0cb3bac4d20ec15be94082019c7ec2f3c86a1f9
hyerin <lhr_id@naver.com>
2018-11-26 19:40:57 +0900

    new name

---
+++ Runnerway/Runnerway/MyHeader.h
@@ -0,0 +1,12 @@
+#pragma once
+//------------Include----------------
+#include <vector>
+using namespace std;
+
+
+//------------Default-----------------
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 700
+
+
+
