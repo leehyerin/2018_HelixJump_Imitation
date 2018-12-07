47e82b579b93c2f15e3809a9873d3dd691b5fbcd
hyerin <lhr_id@naver.com>
2018-12-07 16:33:54 +0900

    +

--- Runnerway/Runnerway/Item.cpp
+++ Runnerway/Runnerway/Item.cpp
@@ -1,7 +1,14 @@
 #include "stdafx.h"
 
+void Item::SetColor(float R, float G, float B)
+{
+	m_Color.x = R;
+	m_Color.y = G;
+	m_Color.z = B;
+}
+
 void Item::Draw()
 {
-	YELLOW;
+	glColor3f(m_Color.x, m_Color.y, m_Color.z);
 	CCube::Draw();
 }
--- Runnerway/Runnerway/Item.h
+++ Runnerway/Runnerway/Item.h
@@ -1,18 +1,22 @@
 #pragma once
 
-enum itemType 
+enum ITEMTYPE
 {
-
+	DASH,
+	BOMB, //반경 (x cm) 다 터뜨리는 거
+	ANSWER
 };
 
 class Item : public CCube
 {
-private:
-	bool type;
+private:	
+	ITEMTYPE m_Type;
+	MyVec m_Color{ 0,0,0 };
 public:
 	Item() {}
 	Item(float x, float y, float z, double r) : CCube(x, y, z, r) {};
 	~Item() {}
 
+	void SetColor(float, float, float);
 	virtual void Draw();
 };
