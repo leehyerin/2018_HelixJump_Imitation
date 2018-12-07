1ac19682ee9a26a9fb9dc76725e438d802abdea2
hyerin <lhr_id@naver.com>
2018-12-07 17:39:14 +0900

    Item.h mod-type init

--- Runnerway/Runnerway/Item.h
+++ Runnerway/Runnerway/Item.h
@@ -15,7 +15,7 @@ private:
 
 public:
 	Item() {}
-	Item(float x, float y, float z, double r) : CCube(x, y, z, r) {};
+	Item(float x, float y, float z, double r, int type) : CCube(x, y, z, r) { m_Type = (ITEMTYPE)type; };
 	~Item() {}
 
 	virtual void Draw();
